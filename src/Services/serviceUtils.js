19a73f8d81b13b0f0e5df8b8d137e15eefcde978
Tyomay1999 <artyom.bordulanyk@gmail.com>
2022-12-30 12:26:52 +0400

    cors errors has been solved.

--- src/Services/serviceUtils.js
+++ src/Services/serviceUtils.js
@@ -1,9 +1,10 @@
 class Service {
     server_url_local = process.env.REACT_APP_SERVER_URL_LOCAL;
     server_url = process.env.REACT_APP_SERVER_URL;
+    server_domain = process.env.REACT_APP_SERVER_URL;
 
     get_server_url() {
-        return this.server_url
+        return this.server_domain
     }
 }
 
