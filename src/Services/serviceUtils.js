08391ea409911673a6f22a828682b6349b081080
Tyomay1999 <artyom.bordulanyk@gmail.com>
2022-12-30 12:31:54 +0400

    small fix.

--- src/Services/serviceUtils.js
+++ src/Services/serviceUtils.js
@@ -1,7 +1,7 @@
 class Service {
     server_url_local = process.env.REACT_APP_SERVER_URL_LOCAL;
     server_url = process.env.REACT_APP_SERVER_URL;
-    server_domain = process.env.REACT_APP_SERVER_URL;
+    server_domain = process.env.REACT_APP_SERVER_DOMAIN;
 
     get_server_url() {
         return this.server_domain
