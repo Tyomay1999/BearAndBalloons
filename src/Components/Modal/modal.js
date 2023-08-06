ce12643f7a2fb82a1e2b631c12aed4daf2b91f74
Tyomay1999 <abordulanyuk@gmail.com>
2023-08-07 00:24:48 +0300

    added modal

--- src/Components/Modal/modal.js
+++ src/Components/Modal/modal.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import modalStyle from './modal.module.scss'
 import bear_gift from '../../Assets/BEAR_GIFT_2.png'
+import Door from '../../Assets/door.jpg'
 
 const Modal = () => {
+
+    const [ see_image, set_see_image_state ] = useState( false )
+
     return <div className={ modalStyle.main }>
-        <div className={modalStyle.modal_container}>
-            <img src={bear_gift} alt="Bear & gift"/>
+        <div className={ modalStyle.modal_container }>
+            <div className={ modalStyle.message_wrapper }>
+                <h1>Find your gift in a real place</h1>
+            </div>
+            <img onClick={() => set_see_image_state(true)} src={ bear_gift } alt="Bear & gift"/>
         </div>
+        {
+            see_image ? <div className={ modalStyle.large_image_box }>
+                <div className={ modalStyle.wrapper }>
+                    <img src={ Door } alt="Gift"/>
+                    <button
+                        onClick={() => {
+                            set_see_image_state(false)
+                        }}
+                        className={ modalStyle.close }><span>X</span></button>
+                </div>
+            </div> : <></>
+        }
+
     </div>
 }
 
