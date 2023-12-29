295c21201aa088c8394d297bc91eb9fb48aa3373
Tyomay1999 <abordulanyuk@gmail.com>
2023-12-30 00:03:11 +0300

    Coming soon.

--- src/Components/Modal/modal.js
+++ src/Components/Modal/modal.js
@@ -8,24 +8,27 @@ const Modal = () => {
     const [ see_image, set_see_image_state ] = useState( false )
 
     return <div className={ modalStyle.main }>
-        <div  className={ modalStyle.modal_container }>
-            {/*<div className={ modalStyle.message_wrapper }>*/}
-            {/*    <h1>Find your gift in a real place</h1>*/}
-            {/*</div>*/}
-            <img onClick={() => set_see_image_state(true)} src={ bear_gift } alt="Bear & gift"/>
-        </div>
-        {
-            see_image ? <div onClick={() => set_see_image_state(false)} className={ modalStyle.large_image_box }>
-                <div onClick={(e) => e.stopPropagation()} className={ modalStyle.wrapper }>
-                    <img src={ Door } alt="Gift"/>
-                    <button
-                        onClick={() => {
-                            set_see_image_state(false)
-                        }}
-                        className={ modalStyle.close }><span>X</span></button>
-                </div>
-            </div> : <></>
-        }
+        <h1>
+            Coming soon
+        </h1>
+        {/*<div  className={ modalStyle.modal_container }>*/}
+        {/*    /!*<div className={ modalStyle.message_wrapper }>*!/*/}
+        {/*    /!*    <h1>Find your gift in a real place</h1>*!/*/}
+        {/*    /!*</div>*!/*/}
+        {/*    <img onClick={() => set_see_image_state(true)} src={ bear_gift } alt="Bear & gift"/>*/}
+        {/*</div>*/}
+        {/*{*/}
+        {/*    see_image ? <div onClick={() => set_see_image_state(false)} className={ modalStyle.large_image_box }>*/}
+        {/*        <div onClick={(e) => e.stopPropagation()} className={ modalStyle.wrapper }>*/}
+        {/*            <img src={ Door } alt="Gift"/>*/}
+        {/*            <button*/}
+        {/*                onClick={() => {*/}
+        {/*                    set_see_image_state(false)*/}
+        {/*                }}*/}
+        {/*                className={ modalStyle.close }><span>X</span></button>*/}
+        {/*        </div>*/}
+        {/*    </div> : <></>*/}
+        {/*}*/}
 
     </div>
 }
