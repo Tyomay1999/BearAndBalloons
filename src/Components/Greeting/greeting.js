e3dd7ab195b36206efadfc35b923bcdd77da9892
Tyomay1999 <abordulanyuk@gmail.com>
2024-08-08 00:58:49 +0300

    Module X.R

--- src/Components/Greeting/greeting.js
+++ src/Components/Greeting/greeting.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import greetingStyle from './greeting.module.scss'
-import bear from "../../Assets/teddy-bear1.png"
+import bear from "../../Assets/bear.png"
+// import bear from "../../Assets/teddy-bear1.png"
 import balloon from "../../Assets/balloon.gif"
 import ViewID from "../ViewID/viewID";
 import Message from "../Message/message";
@@ -9,20 +10,21 @@ import { send_customer_id } from "../../Redux/Actions/common.actions";
 
 const Greeting = () => {
     const dispatch = useDispatch()
-    const common = useSelector(state => state.commonReducer)
+    const common = useSelector( state => state.commonReducer )
 
-        return <div onClick={() => {
-        dispatch(send_customer_id())
-        }
-        } className={greetingStyle.secret}>
-            <div className={greetingStyle.bear_container}>
-                <div className={greetingStyle.image_block}>
-                    <img src={balloon} alt="balloon" />
-                    <img src={bear} alt="bear" />
-                </div>
-                <h1>ME</h1>
+    return <div onClick={ () => {
+        // dispatch( send_customer_id() )
+    }
+    } className={ greetingStyle.secret }>
+        <div className={ greetingStyle.bear_container }>
+            <h1>Happy Birthday</h1>
+            <div className={ greetingStyle.image_block }>
+                {/*<img src={ balloon } alt="balloon"/>*/}
+                <img src={ bear } alt="bear"/>
             </div>
+            <h1></h1>
         </div>
+    </div>
 }
 
 export default Greeting
